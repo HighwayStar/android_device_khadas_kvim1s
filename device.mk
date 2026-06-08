41696ce0e1ac93484b68a883a124522d8d57fec7
Vitaliy Tomin <highwaystar.ru@gmail.com>
2026-06-08 14:48:39 +0800

    kvim1s: Update wifi firmware path
    Change-Id: I545fa7f161b6bdcc1ce7bfa408d90bbb2ef34c5a

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ PRODUCT_PACKAGES += \
     dhd
 
 ## Wi-Fi Firmware
-include hardware/amlogic/kernel-modules/dhd-driver/firmware/firmware.mk
+
+include kernel/amlogic/kernel-modules/dhd-driver/firmware/wifi/wifi.mk
 
 ## Inherit from the common tree product makefile
 $(call inherit-product, device/amlogic/ne-common/ne.mk)
